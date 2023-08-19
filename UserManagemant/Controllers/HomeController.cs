82b61523e4887b15af8ab43c74112a970651333c
jekaH1 <ceyhun100203@gmail.com>
2023-08-19 20:42:44 +0400

    crud half done

--- UserManagemant/Controllers/HomeController.cs
+++ UserManagemant/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.IO;
 using UserManagemant.Database;
 using UserManagemant.Database.Models;
 using UserManagemant.Models;
@@ -12,6 +13,7 @@ namespace UserManagemant.Controllers
     {
         private readonly AppDbContext _appDbContext;
         private readonly UserCode _userCode;
+        private readonly string path = "C:\\Users\\ceyhu\\OneDrive\\Рабочий стол\\sd\\UserManagemant\\wwwroot\\uploads\\";
 
         public HomeController()
         {
@@ -41,7 +43,7 @@ namespace UserManagemant.Controllers
             return View(productViewModels);
         }
 
-        [HttpGet]
+        [HttpGet("admin/employees/add")]
         public IActionResult Add()
         {
             EmployeeAddViewModel employeeAddViewModel = new EmployeeAddViewModel
@@ -52,7 +54,7 @@ namespace UserManagemant.Controllers
             return View(employeeAddViewModel);
         }
 
-        [HttpPost]
+        [HttpPost("admin/employees/add")]
         public IActionResult Add(EmployeeAddViewModel employeeAddVM)
         {
             if (!ModelState.IsValid) { return View(employeeAddVM); }
@@ -66,7 +68,7 @@ namespace UserManagemant.Controllers
                 PIN = employeeAddVM.PIN,
                 Surname = employeeAddVM.Surname,
                 UserCode = _userCode.GenarateAndReturnUniqueUserCode(),
-                ImageURL = employeeAddVM.Image.SaveFile("C:\\Users\\ceyhu\\OneDrive\\Рабочий стол\\sd\\UserManagemant\\wwwroot\\uploads\\"),
+                ImageURL = employeeAddVM.Image.SaveFile(path),
                 isDeleted = false
 
             };
@@ -76,6 +78,66 @@ namespace UserManagemant.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [HttpGet("admin/employees/update/{userCode}")]
+
+        public IActionResult Update(string userCode)
+        {
+            var updatedEmployee = _appDbContext.Employees.FirstOrDefault(x => x.UserCode == userCode);
+            if (updatedEmployee is null) { return NotFound(); }
+
+            EmployeeUpdateViewModel employeeUpdateViewModel = new EmployeeUpdateViewModel
+            {
+                DepartmentId = updatedEmployee.DepartmentId,
+                Name = updatedEmployee.Name,
+                Email = updatedEmployee.Email,
+                FatherName = updatedEmployee.FatherName,
+                ImageURL = updatedEmployee.ImageURL,
+                PIN = updatedEmployee.PIN,
+                Surname = updatedEmployee.Surname,
+                Departments = _appDbContext.Departments.ToList()
+
+            };
+
+            return View(employeeUpdateViewModel);
+        }
+
+        [HttpPost("admin/employees/update/{userCode}")]
+        public IActionResult Update(EmployeeUpdateViewModel exEmployeeUpdateViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(exEmployeeUpdateViewModel);
+
+            var updatedEmployee = _appDbContext.Employees.FirstOrDefault(x => x.UserCode == exEmployeeUpdateViewModel.UserCode);
+            if (updatedEmployee is null) { return NotFound(); }
+
+            string exImageUrl = exEmployeeUpdateViewModel.ImageURL;
+
+
+            updatedEmployee.Surname = exEmployeeUpdateViewModel.Surname;
+            updatedEmployee.FatherName = exEmployeeUpdateViewModel.FatherName;
+            updatedEmployee.Name = exEmployeeUpdateViewModel.Name;
+
+
+            if (employeeUpdateViewModel != null)
+            {
+
+            }
+
+
+            if (employeeUpdateViewModel.Image != null && exImageUrl != null)
+            {
+                var prevImageUrl = path + exImageUrl;
+                System.IO.File.Delete(prevImageUrl);
+            }
+            
+
+            return View();
+        }
+
+
+
+        [HttpPost("admin/employees/delete/{userCode}")]
 
         public IActionResult Delete(string userCode)
         {
---
+++ UserManagemant/ViewModels/Employees/EmployeeUpdateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using UserManagemant.Database.Models;
+
+namespace UserManagemant.ViewModels.Employees
+{
+    public class EmployeeUpdateViewModel
+    {
+        public string UserCode { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FatherName { get; set; }
+        public string PIN { get; set; }
+        public string Email { get; set; }
+        public IFormFile Image { get; set; }
+        public string ImageURL { get; set; }
+
+        public int DepartmentId { get; set; }
+        public List<Department> Departments { get; set; }
+
+
+    }
+}
